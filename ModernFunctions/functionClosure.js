be1b8c724313296a61ab14b012915b5ddfb50ace
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-02-24 11:51:47 -0500

    Encapsulation using closure

--- ModernFunctions/functionClosure.js
+++ ModernFunctions/functionClosure.js
@@ -16,19 +16,41 @@
 // greetJohn();
 // greetAlice();
 
-//*** A better version
+// //*** A better version
 
-function greet(language){
-    const greeting = language === "fr" ? "Bonjoure, " : "Hello, ";
+// function greet(language){
+//     const greeting = language === "fr" ? "Bonjoure, " : "Hello, ";
 
-    return function sayHello(name){
-        console.log(greeting + name);
-    }
+//     return function sayHello(name){
+//         console.log(greeting + name);
+//     }
+// }
+
+// const frenchGreet = greet("fr");
+// const DefaultGreet = greet("en");
+
+// frenchGreet("Parisa");
+// DefaultGreet("John");
+// DefaultGreet("Omer");
+
+//*** Outer Function returning Object of two closure functions (Encapsulation)
+
+function createCounter() {
+    let count = 0; // Private variable
+    return {
+        increment: function () {
+            count++;
+            console.log(`Count: ${count}`);
+        },
+        decrement: function () {
+            count--;
+            console.log(`Count: ${count}`);
+        },
+    };
 }
 
-const frenchGreet = greet("fr");
-const DefaultGreet = greet("en");
+const counter = createCounter();
+counter.increment();
+counter.increment();
+counter.decrement();
 
-frenchGreet("Parisa");
-DefaultGreet("John");
-DefaultGreet("Omer");
