179c59ed4ffd8bf3faf8762897ab07b93e41f4a7
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-02-28 03:28:22 -0500

    Revert "Develop followers count milestone"
    This reverts commit 3be51d7a7cd405ce7bb696655d5ab03869b3b3a4.

--- Functions/functions.js
+++ Functions/functions.js
@@ -13,33 +13,3 @@ function groceryTracker(val1, val2, val3){
     const total = +val1 + +val2 + +val3
     document.getElementById("total").innerText = `The total amount is :$ ${total.toFixed(2)}`;
 }
-
-//__________________________________________________________________
-
-let count = 0; // Initialize count to 0
-
-function increaseCount() {
-  count++;
-  displayCount();
-  checkCountValue();
-}
-
-function displayCount() {
-document.getElementById('countDisplay').innerText=count;
-}
-
-function checkCountValue() {
-  if (count === 10) {
-    alert("Your Instagram post gained 10 followers! Congratulations!");
-  } else if (count === 20) {
-    alert("Your Instagram post gained 20 followers! Keep it up!");
-  }
-}
-
-function resetCount(){
-    count = 0;
-    displayCount();
-    setTimeout(()=>{
-        alert("The followers count has been reset!");
-    }, 1);
-}
