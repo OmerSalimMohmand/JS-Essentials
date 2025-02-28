d51312b9cb70273a0716e60d56b6fc83d3989056
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-02-28 00:11:59 -0500

    Added grocery tracker practice

--- Functions/functions.js
+++ Functions/functions.js
@@ -7,3 +7,9 @@ function calculateArea() {
     let area = length * width; 
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function groceryTracker(val1, val2, val3){
+    // const total = parseFloat(val1) + parseFloat(val2) + parseFloat(val3);
+    const total = +val1 + +val2 + +val3
+    document.getElementById("total").innerText = `The total amount is :$ ${total.toFixed(2)}`;
+}
