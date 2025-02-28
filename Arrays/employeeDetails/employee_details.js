84ae3c1afeb54adf9ebaff78883f43cfcb91ec5f
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-02-27 20:05:47 -0500

    Added EMS

---
+++ Arrays/employeeDetails/employee_details.js
@@ -0,0 +1,32 @@
+const employees = [
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 4, name: 'Hector', age: 29, department: 'HR', salary: 55000 }
+    ];
+
+//If arrow function has body {} for multilines then explicite return statement should be used
+function displayEmployees(){
+    const employeesList = employees.map((employee, index) =>
+        `<p>${employee.id}: ${employee.name}: ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = employeesList;
+}
+
+function calculateTotalSalaries(){
+    const totalSalaries = employees.reduce((total, employee) => total + employee.salary, 0); //0 is the initial total value
+    alert(`Total Salaries: $${totalSalaries}`);
+}
+
+function displayHREmployees() {
+    const hrEmployees = employees.filter(employee => employee.department === 'HR');
+     const hrEmployeesHTML = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.age} - ${employee.department} - $${employee.salary}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = hrEmployeesHTML;
+}
+
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee)
+        document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.age} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    else
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+}
