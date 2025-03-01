9434c8b4b1623606b175d03a724a9bb86b45f1eb
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-03-01 09:37:17 -0500

    Survey form added

--- Functions/functions.js
+++ Functions/functions.js
@@ -43,3 +43,37 @@ function resetCount(){
         alert("The followers count has been reset!");
     }, 1);
 }
+
+//__________________________________________________________________
+
+function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;
+    const userExperience = document.getElementById('Experience').value;
+    
+    alert('Thank you for your valuable feedback');
+
+    document.getElementById('userName').innerText = username;
+    document.getElementById('userAge').innerText = age;
+    document.getElementById('userEmail').innerText = email;
+    document.getElementById('userJob').innerText = job;
+    document.getElementById('userDesignation').innerText = designation;
+    document.getElementById('userProductChoice').innerText = productType;
+    document.getElementById('userFeedback').innerText = feedback;
+    document.getElementById('userExperience').innerText = userExperience;
+
+    document.getElementById('userInfo').style.display = 'block';
+}
+
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
+
+document.addEventListener('keydown', (event) => {
+    if(event.key === 'Enter')
+        submitFeedback();
+});
