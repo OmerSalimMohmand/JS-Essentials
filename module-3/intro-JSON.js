79113f997eceecd60f6d1124069da0f4303ec98e
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-08-22 13:16:33 -0400

    Added intro-JSON

---
+++ module-3/intro-JSON.js
@@ -0,0 +1,25 @@
+// const personJSON = {
+//   "name": "John Doe",
+//   "age": 30,
+//   "email": "john@example.com",
+//   "address": {
+//     "street": "123 Main St",
+//     "city": "Anytown",
+//     "country": "USA"
+//   },
+//   "tags": ["JavaScript", "Node.js", "Web Development"],
+//   "isStudent": false,
+//   "workExperience": null
+// };
+
+// // console.log(personJSON)
+// const jsonString = JSON.stringify(personJSON);
+// console.log(jsonString);
+
+
+// const github_user = {
+//     username: "OmerSalimMohmand",
+//     email: "omersalim.tech@gmail.com"
+// }
+// const jsonString = JSON.stringify(github_user);
+// console.log(jsonString);
