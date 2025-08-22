50c49686be7213f9d76832542fb95fb730d6cf78
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-08-22 19:56:33 -0400

    xhr practice

--- module-3/xhr/health_article.js
+++ module-3/xhr/health_article.js
@@ -1,38 +1,41 @@
-var xhr = new XMLHttpRequest();
-var url = './health_article.json';
+const xhr = new XMLHttpRequest();
+const url = './health_article.json';
+
 xhr.open('GET', url, true);
 xhr.responseType = 'json';
 
 xhr.onload = function(){
-    var articles = xhr.response.articles;
-    var articlesDiv = document.getElementById('articles');
+    const articles = xhr.response.articles;
+    const articlesDiv = document.getElementById('articles');
+
+    // console.log(articles);
 
     articles.forEach(function(article) {
         var articleDiv = document.createElement('div');
         articleDiv.classList.add('article');
   
-        var title = document.createElement('h2');
+        const title = document.createElement('h2');
         title.textContent = article.title;
   
-        var description = document.createElement('p');
+        const description = document.createElement('p');
         description.textContent = article.description;
   
-        var waysHeader = document.createElement('h3');
+        const waysHeader = document.createElement('h3');
         waysHeader.textContent = 'Ways to Achieve:';
   
-        var waysList = document.createElement('ul');
+        const waysList = document.createElement('ul');
         article.ways_to_achieve.forEach(function(way) {
-          var listItem = document.createElement('li');
+          const listItem = document.createElement('li');
           listItem.textContent = way;
           waysList.appendChild(listItem);
         });
   
-        var benefitsHeader = document.createElement('h3');
+        const benefitsHeader = document.createElement('h3');
         benefitsHeader.textContent = 'Benefits:';
   
-        var benefitsList = document.createElement('ul');
+        const benefitsList = document.createElement('ul');
         article.benefits.forEach(function(benefit) {
-          var listItem = document.createElement('li');
+          const listItem = document.createElement('li');
           listItem.textContent = benefit;
           benefitsList.appendChild(listItem);
         });
