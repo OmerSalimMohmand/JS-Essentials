9aac5065468be932f6a7d02a2ea58a49b65758fc
Omer Salim <omersalim.tech@gmail.com>
2025-08-24 00:32:09 -0600

    added axios practice 2

--- module-3/axios.js
+++ module-3/axios.js
@@ -1,9 +1,34 @@
-const axios = require('axios');
+//--- Using Promise
 
-axios.get('https://jsonplaceholder.typicode.com/posts')
-.then((posts)=>{
-    console.log(posts.data);
-})
-.catch((e)=>{
-    console.log(e)
-})
+const { default: axios } = require("axios");
+
+// const axios = require('axios');
+
+// axios.get('https://jsonplaceholder.typicode.com/posts')
+// .then((posts)=>{
+//     console.log(posts.data);
+// })
+// .catch((e)=>{
+//     console.log(e)
+// })
+
+//--- Using async/await
+
+async function getPosts() {
+    try {
+
+        // const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        // const posts = await response.json()
+        // console.log('Response:', posts);
+
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      console.log('Response:', response.data);
+
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+
+getPosts();
+console.log("Program continuous...!");
