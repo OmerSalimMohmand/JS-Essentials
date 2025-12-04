a72aec633eccf6e5beca6595b31ac80f93b4ec3f
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-12-04 02:08:07 -0500

    Added search feature

--- module-3/TodoList/todo_list.js
+++ module-3/TodoList/todo_list.js
@@ -3,6 +3,9 @@ const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
 const clearAllBtn = document.getElementById("clearAllBtn");
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+const searchList = document.getElementById("searchList");
 
 let tasks = [];
 
@@ -41,6 +44,29 @@ function clearAllTasks(){
     displayTasks();
 }
 
+function searchTasks(){
+    const searchText = searchInput.value.trim().toLowerCase();
+    const foundTasks = tasks.filter((task)=>{
+    const taskText = task.text.toLowerCase();
+        return taskText.indexOf(searchText) == -1 ? false : true;
+    });
+    displaySearchTasks(foundTasks);
+}
+
+function displaySearchTasks(foundTasks){
+    searchList.innerHTML = ""
+    const searchTitle = document.createElement('h3');
+    searchTitle.textContent = (foundTasks.length) ? "Similar tasks found:\n" : "No similar tasks found";
+    searchList.appendChild(searchTitle);
+
+    foundTasks.forEach((task, index)=>{
+        const taskHeader = document.createElement("p");
+        taskHeader.textContent = `${index + 1}: ${task.text}`;
+        searchList.appendChild(taskHeader);
+    })
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
-clearAllBtn.addEventListener("click", clearAllTasks)
+clearAllBtn.addEventListener("click", clearAllTasks);
+searchBtn.addEventListener("click", searchTasks)
