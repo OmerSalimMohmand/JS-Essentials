90a88de6d21f14d124f63ad8765fba8e5c457372
OmerSalimMohmand <omersalim.tech@gmail.com>
2025-12-07 19:00:08 -0500

    Added and improved weather report project

---
+++ module-4/weatherReport/weather_report.js
@@ -0,0 +1,25 @@
+function showweatherDetails(event) {
+    console.log(event)
+    event.preventDefault();
+    const city = document.getElementById('city').value;
+    const apiKey = '0772af081699b577ace58a4492842be7';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        if(data.cod == '404')
+            throw new Error(data.message)
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<h2>Weather in ${data.name}</h2>
+                              <p>Temperature: ${data.main.temp} &#8451;</p>
+                              <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Error fetching weather:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>${error.message}. Please try again.</p>`;
+      });
+}
+
+document.getElementById('weatherForm').addEventListener('submit',showweatherDetails );
