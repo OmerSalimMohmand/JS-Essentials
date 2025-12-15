b5d4f808a7cc227a0d5f93e8c5e32a5e26a8aeb1
Omer Salim <omersalim.tech@gmail.com>
2025-12-15 00:13:39 -0700

    added typing speed analysis lab practice

---
+++ module-2/Sring-Math-Date/typingSpeed/typing_speed.js
@@ -0,0 +1,51 @@
+let testText = "The quick brown fox jumps over the lazy dog.";
+let startTime, endTime;
+
+function startTest() {
+  document.getElementById("inputText").value = testText;
+  let userInput = document.getElementById("userInput");
+  userInput.value = "";
+  userInput.readOnly = false;
+  userInput.focus();
+
+  document.getElementById("output").innerHTML = "";
+
+  startTime = new Date().getTime();
+}
+
+function endTest() {
+  endTime = new Date().getTime();
+
+  document.getElementById("userInput").readOnly = true;
+
+  // Calculate time elapsed and words per minute (WPM)
+  let timeElapsed = (endTime - startTime) / 1000; // in seconds
+  let userTypedText = document.getElementById("userInput").value;
+
+  // Split the text using regex to count words correctly
+  let typedWords = userTypedText.split(/\s+/).filter(function (word) {
+    return word !== "";
+  }).length;
+
+  let wpm = 0; // Default value
+
+  if (timeElapsed !== 0 && !isNaN(typedWords)) {
+    wpm = Math.round((typedWords / timeElapsed) * 60);
+  }
+
+  let outputDiv = document.getElementById("output");
+  outputDiv.innerHTML =
+    "<h2>Typing Test Results:</h2>" +
+    "<p>Total Length: " +
+    userTypedText.length +
+    "</p>" +
+    "<p>Words Typed: " +
+    typedWords +
+    "</p>" +
+    "<p>Time Elapsed: " +
+    timeElapsed.toFixed(2) +
+    " seconds</p>" +
+    "<p>Words Per Minute (WPM): " +
+    wpm +
+    "</p>";
+}
